0767c4844bd6e3079361491cda62426ac70b5023
Miref Dev <miref.dev@gmail.com>
2026-03-20 17:48:51 +0700

    <feat> : :tada: adding icon and change name and desc

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -3,23 +3,29 @@ import type { ElectrobunConfig } from "electrobun";
 export default {
 	app: {
 		name: "Muslim Prayer Schedule",
-		identifier: "muslim-prayer-schedule.electrobun.dev",
-		version: "0.0.1",
-
+		identifier: "muslim-prayer-schedule.miref.app",
+		version: "0.0.2",
+		description: "Indonesian Muslim Prayer Schedule"
 	},
 	build: {
 		copy: {
 			"dist/index.html": "views/mainview/index.html",
 			"dist/assets": "views/mainview/assets",
+			"resources/icon.icns": "resources/icon.icns",
+			"resources/icon.png": "resources/icon.png",
+			"resources/icon.ico": "resources/icon.ico",
 		},
 		mac: {
 			bundleCEF: false,
+			icons: "resources/icon.icns",
 		},
 		linux: {
 			bundleCEF: false,
+			icon: "resources/icon.png",
 		},
 		win: {
 			bundleCEF: false,
+			icon: "resources/icon.ico",
 		},
 	},
 
